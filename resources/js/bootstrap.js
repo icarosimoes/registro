86c64f5186ee0ff2f3d9b571d0a0338eae9f772e
Felix Costa <felix@soterotech.com.br>
2023-05-17 16:48:48 -0300

    deploy

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,4 @@
-window._ = require('lodash');
+window._ = require("lodash");
 
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
@@ -7,16 +7,17 @@ window._ = require('lodash');
  */
 
 try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
+  window.Popper = require("popper.js").default;
+  window.$ = window.jQuery = require("jquery");
 
-    require('bootstrap');
+  require("bootstrap");
 } catch (e) {}
 
-require('overlayscrollbars');
-require('../../vendor/almasaeed2010/adminlte/dist/js/adminlte');
-require('../../public/js/custom');
-require('../../public/js/jquery/global');
+require("overlayscrollbars");
+// require('../../vendor/almasaeed2010/adminlte/dist/js/adminlte');
+require("../vendor/js/adminlte");
+require("../../public/js/custom");
+require("../../public/js/jquery/global");
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -24,9 +25,9 @@ require('../../public/js/jquery/global');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-window.axios = require('axios');
+window.axios = require("axios");
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
