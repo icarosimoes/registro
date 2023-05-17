d6da1720bfb06dd2d3efd710533b024c090fdcc8
Felix Costa <felix@soterotech.com.br>
2023-05-17 16:07:45 -0300

    deploy

---
+++ server_deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+echo "Deploying application ..."
+
+# Enter maintenance mode
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+    # Update codebase
+    git fetch origin deploy
+    git reset --hard origin/deploy
+
+    # Install dependencies based on lock file
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+
+    # Migrate database
+    php artisan migrate --force
+
+    # Note: If you're using queue workers, this is the place to restart them.
+    # ...
+
+    # Clear cache
+    php artisan config:clear
+    php artisan cache:clear
+    php artisan view:clear
+    php artisan view:cache
+    php artisan route:clear
+
+    # Reload PHP to update opcache
+    echo "" | sudo -S service php7.4-fpm reload
+    php composer.phar dumpautoload
+# Exit maintenance mode
+php artisan up
+
+echo "Application deployed!!"
