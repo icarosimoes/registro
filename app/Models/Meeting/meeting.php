25953b6588ae991c82899b1e403dc8ad397e6bb5
gitjorginho <40150765+gitjorginho@users.noreply.github.com>
2023-07-05 10:30:32 -0300

    correcao delete

--- app/Models/Meeting/meeting.php
+++ app/Models/Meeting/meeting.php
@@ -4,9 +4,11 @@ namespace App\Models\Meeting;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class meeting extends Model
 {
+    use SoftDeletes;
+
     public function users()
     {
         return $this->BelongsTo('App\Models\User')->withTrashed();
